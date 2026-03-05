4993f780b762f60a2ccb66de71b173c698fd71ff
cyyang <yueyangc67@gmail.com>
2026-03-05 17:11:24 +0800

    添加了SwiGLU

--- data_pipeline/models/blocks/feedforward/feedforward.py
+++ data_pipeline/models/blocks/feedforward/feedforward.py
@@ -6,6 +6,7 @@ class FeedForward(nn.Module):
     # hidden_dim 通常 为 8/3 dim
     def __init__(self, dim: int, hidden_dim: int, multiple_of: int = 256) -> None:
         super().__init__()
+        hidden_dim = (hidden_dim + multiple_of - 1) // multiple_of * multiple_of
         self.w_gate = nn.Linear(dim, hidden_dim, bias=False)
         self.w_up = nn.Linear(dim, hidden_dim, bias=False)
         self.w_down = nn.Linear(hidden_dim, dim, bias=False)
