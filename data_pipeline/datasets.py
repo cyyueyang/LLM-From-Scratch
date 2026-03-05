39cd3317fc153686981be1d290fb07a52c30c958
cyyang <yueyangc67@gmail.com>
2026-03-05 17:09:29 +0800

    添加了SwiGLU

---
+++ data_pipeline/datasets.py
@@ -0,0 +1,14 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import logging
+
+class PackedDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
